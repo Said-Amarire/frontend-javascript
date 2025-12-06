12b6fd14900ba6ebb59aa57df0d2623fd2a7634a
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-12-06 10:21:26 +0100

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -77,12 +77,18 @@ export type Subjects = 'Math' | 'History';
 export function teachClass(todayClass: Subjects): string {
   if (todayClass === 'Math') {
     return 'Teaching Math';
-  } else {
+  } else if (todayClass === 'History') {
     return 'Teaching History';
   }
+  return '';
 }
 
 // Example usage
+
+console.log(createEmployee(200));         // Teacher
+console.log(createEmployee(1000));        // Director
+console.log(createEmployee('$500'));      // Director
+
 console.log(executeWork(createEmployee(200)));  // Getting to work
 console.log(executeWork(createEmployee(1000))); // Getting to director tasks
 
