3d5b8365009fd3ae2ed86715519dbf0050c5e1a0
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-12-06 11:32:06 +0100

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,8 +2,12 @@
 
 namespace Subjects {
   export class Subject {
-    teacher: Teacher;
+    teacher: Teacher | undefined;
 
+    /**
+     * Sets the teacher for this subject
+     * @param teacher - object implementing Teacher interface
+     */
     setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
     }
